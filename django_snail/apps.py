d3350a3c0af1a121ef3e9e4d929e8dfe95163d7b
milano <milslesarik@gmail.com>
2021-08-28 17:33:16 +0200

    initial commit fixes

--- django_snail/apps.py
+++ django_snail/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class DrfXiosConfig(AppConfig):
+class DjangoSnailConfig(AppConfig):
     name = 'django_snail'
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='json-array-file',
+setup(name='django-snail',
       version='0.1',
-      description='Memory efficient JSON Array file writer',
-      url='https://github.com/milano-slesarik/json-array-file',
+      description='Django middleware that mimics slow responses',
+      url='https://github.com/milano-slesarik/django-snail',
       author='Milan Slesarik',
       author_email='milslesarik@gmail.com',
       license='MIT',
-      packages=['json_array_file'],
+      packages=['django_snail'],
       zip_safe=False)
